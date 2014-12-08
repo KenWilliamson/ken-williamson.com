0e6567536aa988b05dced72430604c06979ec7a3
ken <ken@bigMo>
2014-12-07 22:38:04 -0500

    updates

---
+++ starter.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if [ $(ps -e -o uid,cmd | grep $UID | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
+then
+        export PATH=/usr/local/bin:$PATH
+        forever start --sourceDir /var/www/ken-williamson.com server.js >> /var/log/nginx/ken-williamson.com/log.txt 2>&1
+fi
+
